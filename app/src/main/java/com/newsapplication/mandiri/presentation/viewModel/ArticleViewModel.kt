0cc981c1f5d5224259317cbc3610bacb19184efc
Dave Radin <daveputraradin@gmail.com>
2026-06-03 21:19:52 +0700

    finishing all

--- app/src/main/java/com/newsapplication/mandiri/presentation/viewModel/ArticleViewModel.kt
+++ app/src/main/java/com/newsapplication/mandiri/presentation/viewModel/ArticleViewModel.kt
@@ -7,18 +7,13 @@ import androidx.paging.cachedIn
 import com.newsapplication.mandiri.domain.model.ArticleModel
 import com.newsapplication.mandiri.domain.repository.NewsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ArticleViewModel @Inject constructor(
     private val newsRepository: NewsRepository
 ): ViewModel() {
-
-    private var _error = Channel<String>()
-    val error get() = _error.receiveAsFlow()
 
     fun getArticles(source: String, search: String? = null): Flow<PagingData<ArticleModel>> {
         return newsRepository.getArticlesPaging(source, search)
